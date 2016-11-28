730cb0cfb14b09d8240810d3f7afd5ceabab1deb
FPNelson <fpnelson@iastate.edu>
2016-11-28 12:29:34 -0600

    Resolved scheduling issues

--- src/task/TaskInstanceComparator.java
+++ src/task/TaskInstanceComparator.java
@@ -7,6 +7,6 @@ public class TaskInstanceComparator implements Comparator<TaskInstance> {
 	 * Orders the tasks using (m, k)-Firm algorithm, the task with the highest priority goes first.
 	 */
 	public int compare(TaskInstance task1, TaskInstance task2) {
-		return task1.getP() - task2.getP();
+		return task1.isMandatory() ^ task2.isMandatory() ? task1.getP() - task2.getP() : task1.getParent().getP() - task2.getParent().getP();
 	}
 }
